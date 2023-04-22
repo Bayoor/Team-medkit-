d79c78e2561f45ecc6d8bb081bb4892d3ea40a8a
Toby <bayonleadebisijose@gmail.com>
2023-04-23 00:15:12 +0100

    fix: about page

--- src/main.jsx
+++ src/main.jsx
@@ -4,8 +4,6 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import theme from "./style/theme";
 import './index.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.bundle.min';
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
